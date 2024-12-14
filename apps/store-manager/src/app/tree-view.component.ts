50c27609f6bf9fefbd4a50bf4ab60d5bb29ef9d8
Ankur Dutta <ankur.611@gmail.com>
2024-12-14 08:17:07 +0000

    Editor added

--- apps/store-manager/src/app/tree-view.component.ts
+++ apps/store-manager/src/app/tree-view.component.ts
@@ -1,12 +1,14 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { CategoryService } from './category.service';
 import { NgClass, NgTemplateOutlet, TitleCasePipe } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-tree-view',
   template: `
     <ng-template let-category #categoryNode>
       <div class="font-inter p-2">
+        @let state = categoryNode.elementRef.nativeElement.state;
         <div class="cursor-pointer flex gap-4 items-center">
           <div
             class="select-none"
@@ -16,19 +18,49 @@ import { NgClass, NgTemplateOutlet, TitleCasePipe } from '@angular/common';
             "
           >
             @if (category.children.length == 0) { 📦 } @else if
-            (categoryNode.elementRef.nativeElement.state?.[category.id] ==
-            false) { ➕ } @else { ➖ }
+            (state?.[category.id] && state?.[category.id]?.display == false) {
+            ➕ } @else { ➖ }
           </div>
           <div class="font-semibold">
+            @if (state?.[category.id] && state?.[category.id].edit == true) {
+            <input
+              class="border border-gray-400 rounded-lg p-1"
+              type="text"
+              [(ngModel)]="category.name"
+            />
+            } @else {
             {{ category.name | titlecase }}
+            }
           </div>
-          <div class="btn text-sm p-1 border border-gray-400 rounded-lg">
+          @if (state?.[category.id] && state?.[category.id].edit == true) {
+          <div
+            class="btn text-sm p-1 border border-gray-400 rounded-lg"
+            (click)="saveEdit(category, categoryNode.elementRef.nativeElement)"
+          >
+            📩 Save
+          </div>
+          <div
+            class="btn text-sm p-1 border border-gray-400 rounded-lg"
+            (click)="
+              cancelEdit(category, categoryNode.elementRef.nativeElement)
+            "
+          >
+            ✖️ Cancel
+          </div>
+          } @else {
+          <div
+            class="btn text-sm p-1 border border-gray-400 rounded-lg"
+            (click)="
+              editCategory(category, categoryNode.elementRef.nativeElement)
+            "
+          >
             ✏️ Edit
           </div>
+          }
         </div>
         <ul
           class="list-none ml-4 border-l border-gray-300 pl-4 space-y-2"
-          [ngClass]="{ hidden: categoryNode.elementRef.nativeElement.state?.[category.id] == false }"
+          [ngClass]="{ hidden: state?.[category.id] && state?.[category.id].display == false }"
         >
           @for (categoryItem of category.children; track categoryItem.id) {
           <li class="">
@@ -48,7 +80,7 @@ import { NgClass, NgTemplateOutlet, TitleCasePipe } from '@angular/common';
       *ngTemplateOutlet="categoryNode; context: { $implicit: categoryTree() }"
     ></ng-container>
   `,
-  imports: [NgTemplateOutlet, TitleCasePipe, NgClass],
+  imports: [NgTemplateOutlet, TitleCasePipe, NgClass, FormsModule],
 })
 export class TreeViewComponent implements OnInit {
   private readonly categoryService = inject(CategoryService);
@@ -62,7 +94,7 @@ export class TreeViewComponent implements OnInit {
     });
   }
 
-  toggleCategory(category: any, element: any) {
+  initCategoryState(category: any, element: any) {
     if (!category.id) {
       category.id = 'root';
     }
@@ -70,14 +102,34 @@ export class TreeViewComponent implements OnInit {
       element.state = {};
     }
     if (element.state[category.id] == undefined) {
-      if (this.defaultState() == 'open') {
-        element.state[category.id] = true;
-      } else {
-        element.state[category.id] = true;
-      }
+      element.state[category.id] = {
+        display: this.defaultState() == 'open',
+        edit: false,
+      };
     }
-    element.state[category.id] = !element.state[category.id];
+    // console.log(category.id, element.state[category.id]);
   }
 
-  editCategory(category) {}
+  toggleCategory(category: any, element: any) {
+    this.initCategoryState(category, element);
+    element.state[category.id].display = !element.state[category.id].display;
+  }
+
+  editCategory(category: any, element: any) {
+    this.initCategoryState(category, element);
+    element.state[category.id].edit = true;
+  }
+
+  cancelEdit(category: any, element: any) {
+    this.initCategoryState(category, element);
+    element.state[category.id].edit = false;
+  }
+
+  saveEdit(category: any, element: any) {
+    this.initCategoryState(category, element);
+    element.state[category.id].edit = false;
+    // this.categoryService.saveCategory(category).subscribe((res) => {
+    //   console.log(res);
+    // });
+  }
 }
